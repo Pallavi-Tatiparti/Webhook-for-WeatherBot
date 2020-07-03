9628cc84c9b997f5583028aacb3021737ec31728
Pallavi-Tatiparti <ptatiparti@gmail.com>
2020-07-03 22:40:17 +0530

    Webhook configured for api.ai bot

---
+++ webhook.py
@@ -0,0 +1,47 @@
+import json
+import os
+import requests
+
+from flask import Flask
+from flask import request
+from flask import make_response
+
+# flask app should start in global layout
+app = Flask(__name__)
+
+@app.route('/webhook', methods= ['POST'])
+def webhook():
+    req = request.get_json(silent= True, force= True)#requesting data
+    print(json.dumps(req, indent=4))
+
+    res= makeResponse(req)
+    res= json.dumps(req, indent=4)
+
+    r= make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+def makeResponse(req):
+    result = req.get("result")
+    parameters = result.get("parameters")
+    city= parameters.get("geo-city")
+    date= parameters.get("date")
+    r=requests.get('http://samples.openweathermap.org/data/2.5/forecast/hourly?q='+city+',us&mode=xml&appid=439d4b804bc8187953eb36d2a8c26a02')
+    json_object = r.json()
+    weather=json_object['list']
+    for i in range(0,30):
+        if date on weather[i]['dt_txt']:
+            condition=weather[i]['weather'][0]['description']
+            break
+    speech= "The forecast for"+city+"for"+date+"is"+condition
+    return{#dictionary
+    "speech":speech,
+    "displayText":speech,
+    "source":"apiai-weather-webhook"
+        }
+
+if __name__='__main__':
+    port = int(os.getenv('PORT',5000)) #default port on which flask runs
+    print("Starting app on port %d" %port)
+    app.run(debug=False , port=port , host='0.0.0.0')#app.run get flask running on server when we deploy it
+
